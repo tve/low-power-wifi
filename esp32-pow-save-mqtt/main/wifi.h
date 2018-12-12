1e88558ae41119334c140e0e8346f3bb137c2ef5
Thorsten von Eicken <tve@voneicken.com>
2018-12-11 18:22:18 -0800

    add esp32 power-save sketch

---
+++ esp32-pow-save-mqtt/main/wifi.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#if 1
+#define SSID "test"
+#define PASSWD 0
+#elif 1
+#define SSID "tve-home"
+#define PASSWD "tve@home"
+#else
+#define SSID "tve-cell"
+#define PASSWD "tve@cell"
+#endif
+
+#define MQTTIDENT "esp32"
+#define MQTTPSK "055c252dd6f23dcc87a6cda8fc8cdaf6"
